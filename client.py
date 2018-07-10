dca7aff584ccdc2cec18705f9aa794917160a634
nlemk <nils.lemke@outlook.com>
2018-07-10 14:25:20 +0200

    Anfang vom Heartbeat erstellt

--- client.py
+++ client.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
+import json
 import socket
 import platform
 import subprocess
 import re
 import os
+import time 
 from uuid import getnode
+from time import gmtime, strftime
+
+
+dateTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 clientID = str(getnode())
 
@@ -20,7 +26,6 @@ systemInfo = {"system" : system.system(),
 
 cpu = subprocess.Popen(["lscpu | grep -E Arc\|name\|CPU"], stdout=subprocess.PIPE, shell=True) 
 cpu1 = str(cpu.communicate()[0]).replace('\\n','\n')
-#len-2 to remove \n
 cpu1 = cpu1[2: len(cpu1)-2]
 
 cpu1 = cpu1.split('\n')
@@ -49,7 +54,6 @@ for x in range(0, len(cpu1)):
 DEVNULL = open(os.devnull, 'wb')
 ram = subprocess.Popen(["cat /proc/meminfo | grep -E Mem"], stdout=subprocess.PIPE, shell=True)
 ram1 = str(ram.communicate()[0]).replace('\\n','\n')
-#len-2 to remove \n
 ram1 = ram1[2: len(ram1)-2]
 ram1 = ram1.split('\n')
 
@@ -65,11 +69,9 @@ for x in range(0, len(ram1)):
 
 
 gpu = subprocess.Popen(["lshw -c video | grep -E Besch\|Prod\|Herst\|Takt"], stdout=subprocess.PIPE,stderr=subprocess.STDOUT, shell=True)
-#gpuStart = subprocess.Popen(["lshw | grep -n display"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT,shell=True)
 
 gpu1 = str(gpu.communicate()[0]).replace('\\n','\n')
 
-#len-2 to remove \n 
 gpu1 = gpu1[2: len(gpu1)-2]
 gpu1 = gpu1.split('\n')
 
@@ -85,6 +87,17 @@ for x in range(0, len(gpu1)):
 		dictInfo = (gpu1[x][m.span()[1]:]).replace('  ','')
 		gpuInfo.update({dictName : dictInfo})
 
+info = {"Client-ID": clientID,
+	"CPU:Info" : cpuInfo,
+        "RAM-Info" : ramInfo,
+	"GPU-Info" : gpuInfo
+	}
+
+infotext = json.dumps(info)
+#json.loads(infotext)
+
+beatID = json.dumps({"Client-ID": clientID})
+#json.loads(beatID)
 
 host = 'localhost'
 port = 9000
@@ -93,10 +106,18 @@ port = 9000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 s.connect((host,port))
 
-while True:
+accept = False
+while not accept:
 	recievedbytes = s.recv(50)
 	if (len(recievedbytes) == 0):
 		break;
 	print(recievedbytes.decode('utf-8'))
+	accept = True
+
+
+s.send(bytes(infotext,'utf-8'))	
+while True:
+	time.sleep(0.5)
+	s.send(bytes(beatID,'utf-8'))
 
 s.close()
--- server.py
+++ server.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python
 
 import socket
+import json
+import time 
+from threading import Thread 
+
+count = 0
+clientList = []
+
+
+def heartbeat():
+	connected = True
+	global count
+	while connected:
+		print("thread" + str(count))
+		count+=1
+		beat = inSocket.recv(40)
+		if (len(beat) > 0):
+			iD = beat.decode('utf-8')
+			json_iD = json.loads(iD)
+			print(json_info["Client-ID"])
+		else:
+			connected = False
+			clientList.remove(json_info["Client-ID"])
+			print("disconnected")
+		time.sleep(0.5)
 
 host = 'localhost'
 port = 9000
@@ -9,13 +33,31 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 s.bind((host,port))
 s.listen(socket.SOMAXCONN)
 
+connect = False
 try:
-	while True:	
+	while not connect:	
 		inSocket, addr = s.accept();
 		print("Connection with Client {}".format(addr))
 		
 		inSocket.send(bytes("Server accepts connection",'utf-8'))
-		inSocket.close()
+		#inSocket.close()
+		connect = True
+		
+	clientInfo = inSocket.recv(500)		
+	if (len(clientInfo) > 0):
+		info = clientInfo.decode('utf-8')
+		print(info)
+		json_info = json.loads(info)
+		print(json_info["Client-ID"])
+	clientList.append(json_info["Client-ID"])
+	t = Thread(target=heartbeat)
+	t.start()
+	while len(clientList) > 0:
+		print(clientList)
+		time.sleep(4)
+	#print("Wann kommt das hier dran")
+	
+	inSocket.close()
 		
 finally:
 	s.close()
